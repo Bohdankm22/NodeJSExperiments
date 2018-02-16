2cdd28b5d062c3b839110ba11016fd08e3413d2f
Bohdankm22 <bohdansharipov@gmail.com>
2018-02-15 21:54:31 -0500

    Added route to registration in a new file.

---
+++ app/routes/register.server.routes.js
@@ -0,0 +1,13 @@
+﻿//This uses CommonJS module pattern to export a single module function.display
+//This function takes an express object as argument 
+//Then it requires the index controller and uses its render() method
+//as a middleware to GET requests made to the root path.
+
+module.exports = function (app) {
+    //load the controllers
+    var login = require('../controllers/register.server.controller');
+
+
+    //handle the routing of get
+    app.get('/register', login.render);
+};
