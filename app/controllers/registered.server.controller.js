1128c9f57fa3334fd060654fc506006ed17b9665
Bohdankm22 <bohdansharipov@gmail.com>
2018-02-18 11:20:26 -0500

    Changed the function to save the user when registered.

--- app/controllers/registered.server.controller.js
+++ app/controllers/registered.server.controller.js
@@ -1,22 +1,26 @@
 ﻿//This code uses the CommonJS module pattern to define a function named render()
 //You can require this module and use this function
 //You'll need to use Express routing functionality to utilize the controller
-exports.render = function (req, res) {
-    //make a reference to the session object
-    var session = req.session;
-    console.log("In index function - User name = " + session.username);
 
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var email = req.body.email;
-    var username = req.body.username;
-    var feedback = req.body.password;
+var User = require('mongoose').model('User');
+//
+// Create a new 'create' controller method
+exports.create = function (req, res, next) {
+    // Create a new instance of the 'User' Mongoose model
+    var user = new User(req.body);
+    console.log("body: " + req.body.username);
 
+    // Use the 'User' instance's 'save' method to save a new user document
+    user.save(function (err) {
+        if (err) {
+            // Call the next middleware with an error message
+            return next(err);
+        } else {
+            res.render('index', {
+                title: 'Login page'
+            });
 
-    res.render('index', {
-        title: 'Login page'
-
+            console.log("User saved to DB");
+        }
     });
-
-    console.log("GET request to Login");
 };
