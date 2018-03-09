e772f95e6d8073b4fbd51ce69cda13078f2215c5
Bohdankm22 <bohdansharipov@gmail.com>
2018-03-08 21:01:50 -0500

    Added saving feedback in db.

--- app/controllers/thankyou.server.controller.js
+++ app/controllers/thankyou.server.controller.js
@@ -1,6 +1,9 @@
 ﻿//This code uses the CommonJS module pattern to define a function named render()
 //You can require this module and use this function
 //You'll need to use Express routing functionality to utilize the controller
+
+var Feedback = require('mongoose').model('Feedback');
+
 exports.render = function (req, res) {
     //make a reference to the session object
     var session = req.session;
@@ -10,20 +13,32 @@ exports.render = function (req, res) {
     var lname = session.lastName;
     var fpizza = req.body.fpizza;
     var fflower = req.body.fflower;
+    req.username = session.username;
+    var feed = new Feedback(req.body);
 
     console.log("In index function - User name = " + session.username);
     //check if session object contains the username
 
-    //display the ejs page
-    res.render('thankyou', {
-        title: 'Thank you page',
-        feedback: feedback,
-        lname: lname,
-        fname: fname,
-        fpizza: fpizza,
-        fflower: fflower
+    // Use the 'User' instance's 'save' method to save a new user document
+    feed.save(function (err) {
+        if (err) {
+            // Call the next middleware with an error message
+            return next(err);
+        } else {
+            //display the ejs page
+            res.render('thankyou', {
+                title: 'Thank you page',
+                feedback: feedback,
+                lname: lname,
+                fname: fname,
+                fpizza: fpizza,
+                fflower: fflower
+            });
+
+            console.log("Feedback saved to DB");
+        }
     });
-    
+
     console.log("GET request - User name = " + session.username);
 
 };
