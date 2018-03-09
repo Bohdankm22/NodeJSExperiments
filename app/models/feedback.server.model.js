e62e5711a64c5c92ca4216691a32199be799c077
Bohdankm22 <bohdansharipov@gmail.com>
2018-03-08 21:01:20 -0500

    Added feedback model.

---
+++ app/models/feedback.server.model.js
@@ -0,0 +1,12 @@
+﻿// Load the Mongoose module and Schema object
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+// Define a new 'UserSchema'
+var FeedbackSchema = new Schema({
+    username: String,
+    feedback: String,
+    fflower: String,
+    fpizza: String
+});
+// Create the 'User' model out of the 'UserSchema'
+mongoose.model('Feedback', FeedbackSchema);
